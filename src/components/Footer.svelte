00ae3b8865857878b3b3de77900f3ba81fa99cee
Joel Karlsson <thejoelkarlsson@gmail.com>
2020-07-06 23:10:59 +0200

    Add early implementation of talks

---
+++ src/components/Footer.svelte
@@ -0,0 +1,14 @@
+<script>
+  const year = new Date().getFullYear();
+</script>
+
+<style>
+  footer {
+    border-top: 1px solid rgba(0,0,0,0.05);
+    text-align: center;
+  }
+</style>
+
+<footer>
+  <p>Copyright 2019-{year}</p>
+</footer>
