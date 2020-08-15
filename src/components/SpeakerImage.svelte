39004a65802a23a636d2a15a476c138a0a175869
Joel Karlsson <joel.karlsson@cygni.se>
2020-08-15 19:45:23 +0200

    Update talks page

--- src/components/SpeakerImage.svelte
+++ src/components/SpeakerImage.svelte
@@ -6,10 +6,10 @@
 
 </style>
 
-<div class="flex flex-wrap">
+<div class="flex flex-wrap justify-center">
   {#each speakers as speaker}
     <img
-      class={`w-full ${speakers.length > 1 && `sm:w-1/${speakers.length}`}`}
+      class={'rounded-full shadow-lg border-4 border-solid border-white w-1/3'}
       alt={speaker.name}
       src={`/images/speaker/${speaker.image}`} />
   {/each}
