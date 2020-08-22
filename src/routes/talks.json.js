bbcb4833fba04b62f0da70148614e0c5a8ae1616
Joel Karlsson <joel.karlsson@cygni.se>
2020-08-22 11:53:53 +0200

    Update mobile styling

--- src/routes/talks.json.js
+++ src/routes/talks.json.js
@@ -1,6 +1,6 @@
 import json from "Data/talks.json";
 
-export const talks = JSON.stringify(json);
+export const talks = JSON.stringify(json.sort((a, b) => a.title.localeCompare(b.title)));
 
 export function get(req, res) {
   res.writeHead(200, { "Content-Type": "application/json" });
